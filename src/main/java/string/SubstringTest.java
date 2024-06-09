22f9d93880efb5f50f7db8aea76903c459500ab0
rbgusdlza <rbgusdlza@gmail.com>
2024-06-10 02:32:29 +0900

    feat: 학습 테스트 구현

---
+++ src/main/java/string/SubstringTest.java
@@ -0,0 +1,10 @@
+package string;
+
+public class SubstringTest {
+
+    public static void main(String[] args) {
+        String str = "(1,2)";
+        String substring = str.substring(1, 4);
+        System.out.println("substring = " + substring);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,35 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-public class StringTest {
+class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @DisplayName("문자열 범위 밖의 인덱스로 문자열을 참조할 때 에러가 발생한다.")
+    @Test
+    void charAt() {
+        String actual = "abc";
+        assertThatThrownBy(() -> actual.charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
